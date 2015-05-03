cabdb75d6abf4192cd14cd6e13e7aecffc173091
Patrick Buergin <hi@patrickbuergin.com>
2015-05-03 23:15:24 +0200

    removed useless Grid code

--- src/js/factories/Grid.js
+++ src/js/factories/Grid.js
@@ -9,20 +9,13 @@
       self.columns = parseInt(options.columns) || DEFAULT_COLUMNS;
       self.rows = parseInt(options.rows) || DEFAULT_ROWS;
       self.cellSize = gridUtil.computeCellSize(self.rows, self.columns);
-      
-//      var stateGrid = gridUtil.initStateGrid(self.rows, self.columns);
 
       self.widgets = [];
       
       self.add = function (widget) {
         self.widgets.push(widget);
-//        positionWidget(widget, stateGrid);
       };
-      
-//      self.applyStyle = function (widget) {
-//        return applyStyle(widget, self.cellSize.height, self.cellSize.width);
-//      };
-      
+
       self.resize = function (rows, columns) {
         columns = parseInt(columns) || 0;
         rows = parseInt(rows) || 0;
@@ -31,44 +24,9 @@
           self.columns = columns;
           self.rows = rows;
           self.cellSize = gridUtil.computeCellSize(self.rows, self.columns);
-//          
-//          stateGrid = gridUtil.initStateGrid(self.rows, self.columns);
-//          positionWidgets(self.widgets, stateGrid, self.cellSize);
         }
       };
     };
- 
-//    function positionWidget(widget, stateGrid) {
-//      var left = widget.left,
-//          top = widget.top,
-//          right = left + widget.width,
-//          bottom = top + widget.height;
-//      
-//      var currGridCell;
-//      for (var i = top; i < bottom; i++) {
-//        for (var j = left; j < right; j++) {
-//          currGridCell = stateGrid[i][j];
-//          currGridCell.available = false;
-//          currGridCell.occupant = widget;
-//        }
-//      }
-//    };
-//     
-//    function positionWidgets(widgets, stateGrid, cellSize) {
-//      widgets = gridUtil.sortWidgets(widgets);
-//      for (var i = 0; i < widgets.length; i++) {
-//        var widget = widgets[i];
-//        positionWidget(widget, stateGrid);
-//        applyStyle(widget, cellSize.height, cellSize.width);
-//      }
-//    }    
-//    
-//    function applyStyle(widget, cellHeight, cellWidth) {
-//      widget.style.top = (widget.top * cellHeight).toString() + '%';
-//      widget.style.height = (widget.height * cellHeight).toString() + '%';
-//      widget.style.left = (widget.left * cellWidth).toString() + '%';
-//      widget.style.width = (widget.width * cellWidth).toString() + '%';
-//    }
 
     return Grid;
   }]);
