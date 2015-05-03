db50db444a0f6d875db2b1a6ae2fa30b0b5dd951
Patrick Buergin <hi@patrickbuergin.com>
2015-05-04 00:09:57 +0200

    removed stateGrid

--- src/js/services/gridUtil.js
+++ src/js/services/gridUtil.js
@@ -40,21 +40,6 @@
           height: rowCount >= 1 ? (Math.round((100 / rowCount) * 10) / 10) : 0,
           width: columnCount >= 1 ? (Math.round((100 / columnCount) * 10) / 10) : 0
         };
-      },
-      
-      initStateGrid: function (m, n) {
-        var stateGrid = [];
-        for (var i = 0; i < m; i++) {
-          var row = [];
-          for (var j = 0; j < n; j++) {
-            row.push({
-              available: true,
-              occupant: null
-            });
-          }
-          stateGrid.push(row);
-        }
-        return stateGrid;
       }
     };
   });
