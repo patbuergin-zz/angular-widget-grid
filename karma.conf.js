0b563d5a09195b6977667bb30c539e2c6b1adb3c
Patrick Buergin <hi@patrickbuergin.com>
2015-05-04 12:54:16 +0200

    added lodash dev-dep

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/lodash/lodash.js',
       'src/js/bootstrap.js',
       'src/js/**/*.js',
       'test/**/*.js'
