b99c4a2dfc79838ad4422d5cbe856e9fa673a3bf
Patrick Buergin <hi@patrickbuergin.com>
2015-05-04 13:56:54 +0200

    set up basic grunt build

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/**\n * @license <%= pkg.title || pkg.name %> v<%= pkg.version %>\n' +
+      ' * (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+      ' * License: <%= pkg.license %>\n' +
+      ' * <%= pkg.homepage %>\n */\n',
+
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true,
+        sourceMap: true
+      },
+      dist: {
+        src: ['src/js/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: false,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        jasmine: true,
+        globals: {
+          angular: false,
+          console: false,
+          module: false,
+          _: false
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['src/**/*.js', 'test/**/*.js']
+      }
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js',
+        singleRun: true,
+        reporters: ['dots']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'karma']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-karma');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'karma', 'concat', 'uglify']);
+};
