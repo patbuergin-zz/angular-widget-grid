856f89d854bc8c2acf6ccfb00ecf157ca411b256
Patrick Buergin <hi@patrickbuergin.com>
2015-05-08 02:10:36 +0200

    improved basic demo

--- demo/basic.js
+++ demo/basic.js
@@ -1,22 +1,42 @@
 angular.module('widgetGridDemo', ["widgetGrid"])
-.controller('DemoController', function($timeout) {
+.controller('DemoController', function($timeout, $sce) {
   var vm = this;
   
   vm.columns = 12;
   vm.rows = 8;
   
+  vm.editable = false;
+  
   vm.widgets = [{
-    position: { top: 0, height: 4, left: 0, width: 5 },
-    style: { backgroundColor: '#981254' }
+    position: { top: 1, height: 4, left: 1, width: 5 },
+    inner: $sce.trustAsHtml(
+      '<div class="demo-widget-container" style="background-color: #981254;">' +
+        '<h1 class="demo-widget-title">TODO</h1>' + 
+      '</div>'
+    )
   },{
-    position: { top: 4, height: 4, left: 5, width: 7 },
-    style: { backgroundColor: '#ef4512' }
+    position: { top: 5, height: 4, left: 6, width: 7 },
+    inner: $sce.trustAsHtml(
+      '<div class="demo-widget-container" style="overflow: hidden">' +
+       '<iframe frameborder="0" height="100%" width="100%" ' +
+         'src="https://youtube.com/embed/V9tmsIbi4Kw?autoplay=0&controls=0&showinfo=0&autohide=1">' +
+       '</iframe>' +
+      '</div>'
+    )
   },{
-    position: { top: 7, height: 1, left: 2, width: 3 },
-    style: { backgroundColor: '#cc7721' }
+    position: { top: 8, height: 1, left: 3, width: 3 },
+    inner: $sce.trustAsHtml(
+      '<div class="demo-widget-container" style="background-color: #7C0BA5;">' +
+        '<h1 class="demo-widget-title">Hi</h1>' + 
+      '</div>'
+    )
   },{
-    position: { top: 0, height: 2, left: 6, width: 3 },
-    style: { backgroundColor: '#c420c4' }
+    position: { top: 1, height: 2, left: 7, width: 3 },
+    inner: $sce.trustAsHtml(
+      '<div class="demo-widget-container" style="background-color: #A5220B;">' +
+        '<h1 class="demo-widget-title">Hi</h1>' +
+      '</div>'
+    )
   }];
   
   function updateGridSize() {
