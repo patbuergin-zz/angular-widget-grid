e031e80b36f2020784aa1b069a0466c8a609d786
Patrick Buergin <hi@patrickbuergin.com>
2015-12-15 02:46:21 +0000

    Updates

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "widgetGrid.gridUtil",
+      "shortName": "widgetGrid.gridUtil",
+      "type": "service",
+      "moduleName": "widgetGrid",
+      "shortDescription": "Provides utility functions for various library components.",
+      "keywords": "$templatecache api area bottom-right cache cached cell collection column columncount components computecellsize computes count empty findlargestemptyarea finds functions gettemplate getuid grid gridposition gridutil identifier key largest library markup method non-obstructed null position relative rendering retrieves returns row rowcount service single size sizes sorted sorts sortwidgets template templatename templates top-left unique utility widgetgrid widgets"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
