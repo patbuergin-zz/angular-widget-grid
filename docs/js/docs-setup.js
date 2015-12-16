500ece43c546515db8141f483765948b5ba9294f
Patrick Buergin <hi@patrickbuergin.com>
2015-12-16 00:30:28 +0000

    Updates

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -5,12 +5,66 @@ NG_DOCS={
   "pages": [
     {
       "section": "api",
+      "id": "widgetGrid.CellSize",
+      "shortName": "widgetGrid.CellSize",
+      "type": "object",
+      "moduleName": "widgetGrid",
+      "shortDescription": "Describes the relative size of a cell in a grid.",
+      "keywords": "api cell cellsize column columncount count create describes factory grid height instance method object relative row rowcount size widgetgrid width"
+    },
+    {
+      "section": "api",
+      "id": "widgetGrid.Grid",
+      "shortName": "widgetGrid.Grid",
+      "type": "object",
+      "moduleName": "widgetGrid",
+      "shortDescription": "Describes a grid.",
+      "keywords": "add adds api cellsize changes column columns contained count describes grid method object remove removes resize row rows size widget widgetgrid"
+    },
+    {
+      "section": "api",
+      "id": "widgetGrid.GridArea",
+      "shortName": "widgetGrid.GridArea",
+      "type": "object",
+      "moduleName": "widgetGrid",
+      "shortDescription": "Describes a rectangular area in a grid.",
+      "keywords": "api area bottom bottom-right column computes corner create describes empty ends factory getbottom getright getsurfacearea grid gridarea height instance left method object property rectangular returns row start starts surface top top-left widgetgrid width"
+    },
+    {
+      "section": "api",
+      "id": "widgetGrid.GridPoint",
+      "shortName": "widgetGrid.GridPoint",
+      "type": "object",
+      "moduleName": "widgetGrid",
+      "shortDescription": "Describes a point in a grid.",
+      "keywords": "api column describes grid gridpoint left method object point row top widgetgrid"
+    },
+    {
+      "section": "api",
+      "id": "widgetGrid.gridRenderer",
+      "shortName": "widgetGrid.gridRenderer",
+      "type": "service",
+      "moduleName": "widgetGrid",
+      "shortDescription": "Provides methods for rendering grids.",
+      "keywords": "api assigning clashes creates grid gridarea gridrenderer gridrendering grids method methods non-valid position positions render rendering repositioning resolving service unpositioned widgetgrid widgets"
+    },
+    {
+      "section": "api",
       "id": "widgetGrid.gridUtil",
       "shortName": "widgetGrid.gridUtil",
       "type": "service",
       "moduleName": "widgetGrid",
       "shortDescription": "Provides utility functions for various library components.",
-      "keywords": "$templatecache api area bottom-right cache cached cell collection column columncount components computecellsize computes count empty findlargestemptyarea finds functions gettemplate getuid grid gridposition gridutil identifier key largest library markup method non-obstructed null position relative rendering retrieves returns row rowcount service single size sizes sorted sorts sortwidgets template templatename templates top-left unique utility widgetgrid widgets"
+      "keywords": "$templatecache api area bottom-right build cache cached cellsize collection components empty findlargestemptyarea finds functions gettemplate getuid gridarea gridpoint gridutil identifier key largest library markup method ngtemplates non-obstructed null populated position rendering retrieve returns service sorted sorts sortwidgets template templatename top-left unique utility widgetgrid widgets"
+    },
+    {
+      "section": "api",
+      "id": "widgetGrid.Widget",
+      "shortName": "widgetGrid.Widget",
+      "type": "object",
+      "moduleName": "widgetGrid",
+      "shortDescription": "Describes a widget container.",
+      "keywords": "api container describes getposition gridarea gridutil method object position setposition updates widget widgetgrid"
     }
   ],
   "apis": {
